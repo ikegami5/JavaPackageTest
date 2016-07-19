63a8c04d5f4ebf7b0c174d49ec04ac76931222c4
ikegami5 <important2nd@gmail.com>
2016-07-19 20:27:31 +0900

    fixed

--- TestMain.java
+++ TestMain.java
@@ -1,8 +1,13 @@
+import dir1.Vector;
+
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 
 		Point point = new Point(2.3, 3);
 		System.out.println(point);
+
+		Vector vector = new Vector(2, 3.6);
+		System.out.println(vector);		
 	}
 }
